2c09c7bd62d5b1a6ebb41d0b83d3e5d354c5ee45
ellurusaihrieshikesh <elluru.saihrieshikesh@gmail.com>
2025-06-25 11:56:32 +0530

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ProductEntryForm from '../components/ProductEntryForm'
 import ProductList from '../components/ProductList'
